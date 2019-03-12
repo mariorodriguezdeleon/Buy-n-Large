83492547ca21de1997b3d5175b1a4be7bb568494
Mario Rodriguez <mrodrigu@northseattle.edu>
2019-03-11 19:06:52 -0700

    update: user prompt and input validation

--- buyNlargeCustomer.js
+++ buyNlargeCustomer.js
@@ -1,9 +1,23 @@
-console.log("This is running");
+// console.log("This is running");
 
 const mysql = require('mysql');
+const inquire = require('inquirer')
 const figlet = require('figlet');
 const fs = require('fs');
-const cTable = require('console.table');
+
+let inventory = [];
+let questions = [
+    {
+        type: 'input',
+        name: 'product_ID',
+        message: 'Please endter the product ID: '
+    },
+    {
+        type: 'input',
+        name: 'quantity',
+        message: 'Please enter the number of units to purchase: '
+    }
+];
 
 let connection = mysql.createConnection({
     host: 'localhost',
@@ -14,22 +28,59 @@ let connection = mysql.createConnection({
     insecureAuth: true
 });
 
-function tableBoard() {
-    //TODO: add the figlet logic and then call the store
+function appEntry() {
+    figlet('Welcome to Buy-N-Large', function(err, data) {
+        if (err) {
+            console.log('Something went wrong...');
+            console.dir(err);
+            return;
+        }
+        console.log(data);
+        returnAll();
+    });
 }
 
 function returnAll() {
+
     connection.connect(function(error){
         if (error) throw error;
         console.log ('Connected as id ' + connection.threadId);
     
-        connection.query('SELECT * FROM products;', function(error, results) {
+        connection.query('SELECT * FROM products;', function(error, res) {
             if (error) throw error;
-            console.table(results);
-    
-            connection.end();
+            // console.log(res)
+            inventory = res;
+            console.table(res);
+            console.table(inventory);
+            appStore();
+            // connection.end();
         });
     });
 }
 
-returnAll();
+function appStore() {
+    // console.log('This will be more infor for the user');
+    console.log('');
+    inquire.prompt(questions).then (answers => {
+
+        for (let i = 0; i < inventory.length; i++) {
+            if (answers.product_ID === inventory[i].item_id.toString()) {
+                console.log('This is the product_ID entered: ' + inventory[i].item_id);
+                if (inventory[i].stock_quantity === 0 || inventory[i].stock_quantity < answers.quantity){
+                    //TODO: Update the table to reflect the items purchased
+                    console.log('There is not enough inventory to fullfil your request. There is only ' + inventory[i].stock_quantity + ' units available');
+                } else{
+                    console.log('There are ' + inventory[i].stock_quantity);
+                }
+            }
+        }
+        
+        console.log(answers.product_ID + " " + answers.quantity);
+
+        connection.end();
+
+        //TODO: Add logic to other functions to query the database
+    });
+}
+
+appEntry();
